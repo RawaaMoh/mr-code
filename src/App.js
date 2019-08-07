b1621587db1423f3182abc0fdbaaa43a0a102853
Rawaa Rajab <rawaa.rajab@gmail.com>
2019-08-07 18:58:58 +0300

    modal
    the modal is working now

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Intro from './components/Intro';
 import Container from './components/Container';
 import Projects from './container/Projects';
 import Footer from './components/Footer';
-import Forms from './components/Forms';
 
 import {
   firstName,
@@ -25,7 +24,6 @@ function App() {
       justifyContent="space-between"
     >
       <Container>
-        <Forms/>
         <Header name={fullName} links={links} />
         <Intro name={firstName} aboutLinkHref={links[1].href} />
         <Projects />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,43 +4,102 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import styles from './index.module.scss';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
 
 
-function Header({ name, links }) {
-  return (
-    <Navbar style={{ backgroundColor: 'pink' }} sticky="top" collapseOnSelect expand="lg" bg="blue" variant="blue">
-      <Navbar.Brand href="#home">MR CODE</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">LESSONS</Nav.Link>
-          <Nav.Link href="#pricing">GAMES</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">MR CODE</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">LESSONS</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">GAMES</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">ABOUT US</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Button className={styles.button}>
-            <Nav.Link className={styles.NavLink} href="#deets">
-              SIGN IN
-              </Nav.Link>
-          </Button>
-          <Button className={styles.button}>
-            <Nav.Link className={styles.NavLink} eventKey={2} href="#memes">
-              SIGN UP
-      </Nav.Link>
-          </Button>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-
-  )
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
 }
 
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
 
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 4),
+  },
+}));
+
+function Header({ name, links }) {
+
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+
+  return (
+    <div>
+      <Navbar style={{ backgroundColor: 'pink' }} sticky="top" collapseOnSelect expand="lg" bg="blue" variant="blue">
+        <Navbar.Brand href="#home">MR CODE</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#features">LESSONS</Nav.Link>
+            <Nav.Link href="#pricing">GAMES</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">MR CODE</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">LESSONS</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">GAMES</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">ABOUT US</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Button onClick={handleOpen} className={styles.button}>
+              <Nav.Link className={styles.NavLink} href="#signin">
+                SIGN IN
+              </Nav.Link>
+            </Button>
+            <Button onClick={handleOpen} className={styles.button}>
+              <Nav.Link className={styles.NavLink} eventKey={2} href="#signup">
+                SIGN UP
+         </Nav.Link>
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+      </Modal>
+    </div>
+  )
+
+}
 export default Header;
+  // export default SimpleModal;
 
