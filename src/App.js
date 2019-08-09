6de25d081e98c5aceae06fee7732105759d8a900
Rawaa Rajab <rawaa.rajab@gmail.com>
2019-08-09 14:44:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from '@material-ui/core/Box';
+import Header from './components/Header';
+import Intro from './components/Intro';
+import Container from './components/Container';
+import Projects from './container/Projects';
+import Footer from './components/Footer';
+
+import {
+  firstName,
+  fullName,
+  links,
+  socialLinks
+} from './constants'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
 
-      {/* import Page from './components/page'
-
-export default Page */}
-    </div>
   );
 }
 
